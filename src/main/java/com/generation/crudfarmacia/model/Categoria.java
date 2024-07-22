63590c6838062a78ec904e00e8efb9684ddf725d
Matheus Eugenio <math.eugenioti@gmail.com>
2024-07-22 10:57:26 -0300

    feat: Implementação do CRUD de Categoria

---
+++ src/main/java/com/generation/crudfarmacia/model/Categoria.java
@@ -0,0 +1,44 @@
+package com.generation.crudfarmacia.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "O atributo nome é obrigatório")
+    private String nome;
+
+    @Size(min = 20, max = 3000, message = "A descrição da categoria precisar ter no mínimo 20 e no máximo 3000 caracteres")
+    private String descricao;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
